49b0b917b7c203ab5a43b5d2a1a6d89d53f6ff71
giphub <giphub@163.com>
2015-07-28 05:55:43 +0800

    add run_test

--- run_test.py
+++ run_test.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
     
     suite.addTest(ServerTest("sample_test"))
     suite.addTest(ServerTest("article_get_by_id_test"))
+    suite.addTest(ServerTest("article_search_by_keyword_in_title"))
 
     unittest.TextTestRunner(verbosity=2).run(suite)
     
